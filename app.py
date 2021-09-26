f4ff69e92c60af17da56c6323197e1a53e2239bf
ReshmaRegijc <reshmaregi.jc@gmail.com>
2021-09-26 23:09:21 +0530

    Books Store

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from myprojects import app
 from flask import render_template
 
 
-app.route('/')
+@app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('home.html')
 
 if __name__=='__main__':
     app.run(debug=True)
--- myprojects/__init__.py
+++ myprojects/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 import os
-from flask_sqlalchemy import SQLAlchemy, sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 
